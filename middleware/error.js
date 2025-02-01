eafa0862e7132101115277eabcc5f8c49c9eaa6d
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2025-02-01 15:07:24 -0800

    Implemented duplicate field error

--- middleware/error.js
+++ middleware/error.js
@@ -6,12 +6,18 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   // Log to console for developer
-  console.log(err.stack);
+  console.log(err.stack); 
 
   // Mongoose bad ObjectId
   if (err.name === 'CastError') {
     const message = `Bootcamp not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
