49002453f27f1395c4968e3179482ea4884fb41c
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2025-02-08 19:28:38 -0800

    Implemented role authorization

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -16,7 +16,7 @@ const courseRouter = require('./courses');
 
 const router = express.Router();
 
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
@@ -25,12 +25,12 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router.route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(protect, createBootcamp);
+  .post(protect, authorize('publisher','admin'), createBootcamp);
 
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(protect, updateBootcamp)
-  .delete(protect, deleteBootcamp);
+  .put(protect, authorize('publisher','admin'), updateBootcamp)
+  .delete(protect, authorize('publisher','admin'), deleteBootcamp);
 
 module.exports = router;
