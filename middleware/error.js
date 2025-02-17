dc4d520c1aa8e06945d9197097679349aeb6e240
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2025-02-16 17:27:30 -0800

    Implemented getting single review

--- middleware/error.js
+++ middleware/error.js
@@ -6,23 +6,23 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   // Log to console for developer
-  console.log(err.stack); 
+  console.log(err.stack);
 
   // Mongoose bad ObjectId
   if (err.name === 'CastError') {
-    const message = `Bootcamp not found with id of ${err.value}`;
+    const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
   }
 
   // Mongoose duplicate key error
   if (err.code === 11000) {
-    const message = "Duplicate field value entered";
+    const message = 'Duplicate field value entered';
     error = new ErrorResponse(message, 400);
   }
 
   // Mongoose validation error
   if (err.name === 'ValidationError') {
-    const message = Object.values(err.errors).map(val => val.message);
+    const message = Object.values(err.errors).map((val) => val.message);
     error = new ErrorResponse(message, 400);
   }
 
